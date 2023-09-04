fda786f2ac6b8fa9da43cb4bc50991bdd2a972fb
2611saurabh <saurabh.kr026@gmail.com>
2023-09-04 10:30:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      <div className="Main">
-      <h1>FAR AWAY ahd djd </h1>
-      </div>
-     
-      <div className="div-2">
-        
-        <div className="wrraper">
-          <p>What do you need for your trip ?</p>
-          <input type="number" className='num'/>
-          <input type="text" className='text-1'/>
-          <button>Add</button>
-        </div>
-      </div>
+export default function App() {
+  return(
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
-    
-  );
+  )
 }
 
-export default App;
+
+
+//making compoment for webpage
+
+
+function Logo(){
+  return <h1>🌴 Far Away 💼</h1>
+}
+
+
+function Form() {
+  return(
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip ?</h3>
+    </div>
+  )
+}
+
+function PackingList() {
+  return (
+    <div className="list"></div>
+  )
+}
+
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You Have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
+
+
+
